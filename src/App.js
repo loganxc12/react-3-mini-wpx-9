1a89e1780a2514fdfe595dce25eeb8a5aec948e5
B-eck37 <brent.eckert7@gmail.com>
2018-08-12 10:31:30 -0700

    Need Post Image

--- src/App.js
+++ src/App.js
@@ -23,116 +23,166 @@ class App extends Component {
           oneGod: {},
           oneGodPowers: [],
           editName: false,
-          name: ''
+          name: '',
+          origin: '',
+          demigod: false,
+          image: '',
+          power: '',
+          create: false
       }
       this.getGods = this.getGods.bind(this);
       this.updateGod = this.updateGod.bind(this);
+      this.createGod = this.createGod.bind(this);
+      this.deleteGod = this.deleteGod.bind(this);
       this.updateName = this.updateName.bind(this);
-      this.nameInput = React.createRef();
+      this.initiateCreate = this.initiateCreate.bind(this);
+      this.reset = this.reset.bind(this);
+
   }
 
   componentDidMount(){
-    // axios.get(baseUrl, apiConfig).then(res => {
-    //   console.log(res)
-    //   this.setState({
-    //     gods: res.data,
-    //     oneGod: []
-    //   })
-    // })
-
-    console.log(apiConfig)
-    axios.post('https://ancient-gods-api.now.sh/api/gods/reset', {}, apiConfig).then(res => {
+    axios.get(baseUrl, apiConfig).then(res => {
       this.setState({
-        gods: res.data
+        gods: res.data,
+        oneGod: [],
+        create: false
       })
     })
   }
 
-getGods(){
-  console.log('hit')
-  // axios (GET)
-  axios.get(baseUrl, apiConfig).then(res => {
-    console.log(res)
-    this.setState({
-      gods: res.data,
-      oneGod: []
+  getGods(){
+    // axios (GET)
+    axios.get(baseUrl, apiConfig).then(res => {
+      this.setState({
+        gods: res.data,
+        oneGod: [],
+        create: false,
+      })
     })
-  })
-  // setState with response -> gods
-}
+    // setState with response -> gods
+  }
 
-getOneGod(id){
-  // axios (GET)
-  console.log('one god hit', id)
-  axios.get(`${baseUrl}/${id}`, apiConfig).then(res => {
-    let oneGodPowers = res.data.powers.map((power, i) => {
-      return (
-       <h4 key={i}>{power}</h4>
-      )
+  getOneGod(id){
+    // axios (GET)
+    console.log('one god hit', id)
+    axios.get(`${baseUrl}/${id}`, apiConfig).then(res => {
+      console.log('data', res.data)
+      let oneGodPowers;
+      if(typeof(res.data.powers) !== "string"){
+        console.log( )
+          oneGodPowers = res.data.powers.map((power, i) => {
+            return (
+            <h4 key={i}>{power}</h4>
+            )
+          })
+      } else {
+        oneGodPowers = res.data.powers
+      }
+      this.setState({
+        oneGod: res.data,
+        oneGodPowers: oneGodPowers,
+        gods: []
+      })
     })
-    this.setState({
-      oneGod: res.data,
-      oneGodPowers: oneGodPowers,
-      gods: []
+    // setState with response -> oneGod
+  }
+
+  updateGod(){
+    const { name } = this.state
+    const { id } = this.state.oneGod
+    // axios (PATCH)
+    axios.patch(`${baseUrl}/${id}`, {name}, apiConfig).then(res => {
+      let oneGodPowers;
+      if(res.data.powers){
+        oneGodPowers = res.data.powers.map((power, i) => {
+          return (
+          <h4 key={i}>{power}</h4>
+          )
+        })
+      }
+      this.setState({
+        oneGod: res.data,
+        editName: false,
+        oneGodPowers: oneGodPowers || [],
+        name: ''
+      })
     })
-  })
-  // setState with response -> oneGod
-}
+    // setState with ????????????????
+  }
 
-updateGod(){
-  const { name } = this.state
-  const { id } = this.state.oneGod
-  // console.log(value, id)
-  // axios (PATCH)
-  axios.patch(`${baseUrl}/${id}`, {name}, apiConfig).then(res => {
-    this.setState({
-      oneGod: res.data,
-      editName: false,
-    })
-  })
-  // setState with ????????????????
-}
-
-createGod(){
-  // axios (POST)
-  // setState with ????????????????
-}
-
-deleteGod(id){
-  // axios (DELETE)
-  console.log(id)
-  axios.delete(baseUrl + `/${id}`, apiConfig).then(res => {
-    this.setState({
-      gods: res.data,
-      oneGod: {}
-    })
-  })
-  // setState with ?????????????????
-}
-
-handleUserInput (e) {
-  const name = e.target.name;
-  const value = e.target.value;
-  this.setState({[name]: value});
-}
-
-updateName(){
-  this.setState(prevState => {
-    return {
-      editName: !prevState.editName
+  createGod(){
+    let newGod = {
+      name: this.state.name,
+      mythology: this.state.origin,
+      demigod: this.state.demigod,
+      image: this.state.image,
+      powers: this.state.power
     }
-  })
-}
+    // axios (POST)
+    axios.post(baseUrl, newGod, apiConfig).then(res => {
+      this.setState({
+        gods: res.data,
+        create: false, 
+        name: '',
+        origin: '',
+        demigod: '',
+        image: '',
+      })
+    })
+    // setState with ????????????????
+  }
+
+  deleteGod(id){
+    // axios (DELETE)
+    axios.delete(baseUrl + `/${id}`, apiConfig).then(res => {
+      this.setState({
+        gods: res.data,
+        oneGod: {}
+      })
+    })
+    // setState with ?????????????????
+  }
+
+  initiateCreate(){
+    this.setState({
+      create: true,
+      gods: [],
+      oneGod: [],
+    })
+  }
+
+  handleUserInput (e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({[name]: value});
+  }
+
+  updateName(){
+    this.setState(prevState => {
+      return {
+        editName: !prevState.editName
+      }
+    })
+  }
+
+  reset(){
+    axios.post('https://ancient-gods-api.now.sh/api/gods/reset', {}, apiConfig).then(res => {
+      this.setState({
+        gods: res.data,
+        create: false
+      })
+    })
+  }
 
 
   render() {
     console.log(this.state)
-    const { oneGod, oneGodPowers, editName } = this.state;
+    const { oneGod, oneGodPowers, editName, create } = this.state;
 
     const gods = this.state.gods.map(god => {
       return (
         <div key={god.id} className='gods'>
-          <h2>{god.name}</h2>
+          <h1>{god.name}</h1>
           <img src={god.image} alt={god.name}  onClick={() => this.getOneGod(god.id)}/>
         </div>
       )
@@ -140,34 +190,85 @@ updateName(){
     
     return (
       <div className="App">
-        <div>
+        <div className='container'>
           <header>
             <h1>The Great Gods</h1>
 
-            <button onClick={this.getGods}>GET the Gods</button>
-            <button onClick={() => this.setState({gods: [], oneGod: []})}>Reset</button>
+            {oneGod.name && <button onClick={this.getGods}>GET Gods</button>}
+            {create && <button onClick={this.getGods}>GET Gods</button>}
+            <button className='reset' onClick={this.reset}>Reset</button>
+            <button onClick={this.initiateCreate}>POST God</button>
           </header>
 
           <div className='gods-parent'>
             {gods}
           </div>
 
-        {oneGod.name ? 
+{/* //////////////// One God Section //////////////////////////// */}
+        {oneGod.name &&
           <div className="one-god">
-          {/* ref={name => this.name = name} */}
-              {editName ? 
-                <input type="text" placeholder={oneGod.name} name='name' value={this.state.name} onChange={e => this.handleUserInput(e)}/> 
-                  : 
-                <h1>{oneGod.name}<i className="fas fa-pen" onClick={this.updateName}></i></h1>
-              }
-              <h4>{oneGod.mythology} {oneGod.demigod ? "Demigod" : "God"}</h4>
-              <img src={oneGod.image} alt={oneGod.name}/>
-              <h2><u>Powers of {oneGod.name}</u></h2>
-              {oneGodPowers}
 
-              <button onClick={this.updateGod}>Update God</button>
+              { editName ?
+                  <div className='update-flex'>
+                    <input 
+                    type="text" 
+                    placeholder={oneGod.name} 
+                    name='name' 
+                    value={this.state.name} 
+                    onChange={e => this.handleUserInput(e)}
+                    /> 
+                    <button onClick={this.updateGod}>Update God</button>
+                  </div>
+                  :
+                  <h1>{oneGod.name}<i className="fas fa-pen" onClick={this.updateName}></i></h1>
+              }
+
+              <h4>{oneGod.mythology} {oneGod.demigod ? "Demigod" : "God"}</h4>
+
+              <img src={oneGod.image} alt={oneGod.name}/>
+
+              <h2><u>Powers of {oneGod.name}</u></h2>
+
+              <div>
+
+              {oneGodPowers}
+              </div>
+
+              
+
               <button onClick={() => this.deleteGod(oneGod.id)}>Delete God</button>
-          </div> : null} 
+
+          </div> 
+        } 
+{/* //////////////// End One God Section //////////////////////////// */}
+
+{/* //////////////// Create God Section ///////////////////////////// */}
+        { create && 
+            <div className='create-flex'>
+              <div className="create-inputs">
+                <label>Name:</label>
+                <input type="text" name='name' value={this.state.name} onChange={e => this.handleUserInput(e)}/>
+                <label>Origin:</label>
+                <input type="text" name='origin' value={this.state.origin} onChange={e => this.handleUserInput(e)}/>
+                <label>Image:</label>
+                <input type="text" name='image' value={this.state.image} onChange={e => this.handleUserInput(e)}/>
+                <label>Power:</label>
+                <input type="text" name='power' value={this.state.power} onChange={e => this.handleUserInput(e)}/>
+                <div>
+                  <label>Demigod:</label>
+                  <input type="radio" onClick={() => this.setState({demigod: true})}/>
+                </div>
+                <button onClick={this.createGod}>Create</button>
+              </div>
+              {this.state.image ? <img src={this.state.image} alt={this.state.name}/> : <img src="" alt=""/> }
+            </div> 
+        }
+
+
+{/* //////////////// End Create God Section ///////////////////////////// */}
+
+          <h3>&copy; DevMountain. All rights reserved.</h3>
+
         </div>
       </div>
     );
